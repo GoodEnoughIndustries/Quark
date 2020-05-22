29cdb1203564cbc1199406d57bf58c08fb4a7dee
Chad Jessup <chad.jessup@gmail.com>
2020-05-22 02:47:31 -0700

    empty projects

---
+++ src/Quark.CLI/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Quark.CLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => Console.WriteLine("Hello World!");
+    }
+}
